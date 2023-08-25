64689922bb62c886f65505e93636f98e7e5610cd
Jack Arthur Harrhy <me@jackharrhy.com>
2023-08-25 10:05:30 -0230

    basic template support using html/template

--- pkg/devserver/server.go
+++ pkg/devserver/server.go
@@ -3,6 +3,8 @@ package devserver
 import (
 	"errors"
 	"fmt"
+	"html/template"
+	"io"
 	"log/slog"
 	"net/http"
 	"path"
@@ -31,17 +33,66 @@ func (s *Server) Start() error {
 	return nil
 }
 
+type PageTemplate struct {
+	Title   string
+	Content string
+}
+
+var pageTemplateContent = `<!DOCTYPE html>
+<html>
+	<head>
+		<title>{{ .Title }}</title>
+		<link rel="stylesheet" href="/assets/css/main.css">
+	</head>
+	<body>
+		<h1>{{ .Title }}</h1>
+		{{ .Content }}
+	</body>
+</html>`
+
+var pageTemplate *template.Template = nil
+
+type PageData struct {
+	Title   string
+	Content template.HTML
+}
+
+func init() {
+	var t, err = template.New("page").Parse(pageTemplateContent)
+	if err != nil {
+		panic(err)
+	}
+	pageTemplate = t
+}
+
+type Renderer struct{}
+
+func (r *Renderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	if name != "page" {
+		return fmt.Errorf("unknown template: %s", name)
+	}
+
+	return pageTemplate.Execute(w, data)
+}
+
 func (s *Server) servePage(c echo.Context) error {
-	file, err := content.ParseFile(path.Join(s.config.ContentDir, c.Param("page")+".md"))
+	page := c.Param("page")
+
+	file, err := content.ParseFile(path.Join(s.config.ContentDir, page+".md"))
 	if err != nil {
 		return fmt.Errorf("error processing file: %w", err)
 	}
 
-	return c.HTMLBlob(http.StatusOK, file.ParsedContent)
+	return c.Render(http.StatusOK, "page", PageData{
+		Title:   page,
+		Content: template.HTML(string(file.ParsedContent)),
+	})
 }
 
 func NewServer(config Config) *Server {
 	echoInst := echo.New()
+
+	echoInst.Renderer = &Renderer{}
 
 	echoInst.Use(slogecho.New(slog.Default()))
 
@@ -49,6 +100,8 @@ func NewServer(config Config) *Server {
 		echoInst: echoInst,
 		config:   config,
 	}
+
+	echoInst.Static("/assets", "assets")
 
 	echoInst.GET("/:page", server.servePage)
 
